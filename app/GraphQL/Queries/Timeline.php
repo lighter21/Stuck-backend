8f7af334eac537dd3094af2201c725bfc8f38054
Bartosz Kilan <bartoszkilan@wp.pl>
2022-02-26 13:37:30 +0100

    walidacja, poprawki timeline

--- app/GraphQL/Queries/Timeline.php
+++ app/GraphQL/Queries/Timeline.php
@@ -8,15 +8,25 @@ use App\Models\User;
 class Timeline
 {
     /**
-     * @param  null  $_
-     * @param  array<string, mixed>  $args
+     * @param null $_
+     * @param array<string, mixed> $args
      */
     public function __invoke($_, array $args)
     {
         $user = User::find($args['user_id']);
-        $postIds = $user->friends->pluck('id')->toArray();
-        array_push($postIds, $user->id);
+        $friendsIds = $user->friends->pluck('id')->toArray();
+        $groupIds = $user->groups->pluck('id')->toArray();
+        array_push($friendsIds, $user->id);
 
-        return Post::whereIn('user_id', $postIds)->with(['comments', 'groups', 'likes', 'user', 'image'])->latest()->get();
+
+        $friendsPosts = Post::whereIn('user_id', $friendsIds)->with(['comments', 'groups', 'likes', 'user', 'image'])->whereDoesntHave("groups")->latest()->get();
+        $postsFromGroups = Post::whereHas('groups', function ($query) use ($groupIds) {
+            return $query->whereIn('id', $groupIds);
+        })->with(['comments', 'groups', 'likes', 'user', 'image'])->latest()->get();
+
+
+        $collection = $friendsPosts->merge($postsFromGroups);
+
+        return $collection;
     }
 }
